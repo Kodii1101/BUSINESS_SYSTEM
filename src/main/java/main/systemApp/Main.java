592464ef05e8484005441dc703b12d07216f27a0
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2025-02-28 12:41:40 +0800

    Successfully integrated Argon2 via Maven!

--- src/main/java/main/systemApp/Main.java
+++ src/main/java/main/systemApp/Main.java
@@ -1,7 +1,13 @@
 package main.systemApp;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        char[] password = "mySecurePassword".toCharArray();
+        System.out.println("Hello, ARGON2!");
+        Argon2 argon2 = Argon2Factory.create();
+        String hash = argon2.hash(10, 65536, 1, password);
+        System.out.println("Hashed Password: " + hash);
     }
 }
